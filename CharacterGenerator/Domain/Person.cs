36dcf06d4381605909a65583b0afab4abe78301a
Brian Bowen <brianbowen1999@gmail.com>
2020-02-26 00:08:48 -0600

    Finsished attribute and class structure for Person Class

--- CharacterGenerator/Domain/Person.cs
+++ CharacterGenerator/Domain/Person.cs
@@ -6,5 +6,46 @@ namespace CharacterGenerator
 {
     class Person
     {
+        // Person Class Attributes
+        // TODO: Define each getter and setter for proper error checking
+        //Physical Characteristics
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string HairColor { get; set; }
+        public string EyeColor { get; set; }
+        public string SkinComplection { get; set; }
+        public char Gender { get; set; }
+
+        //Other important information
+        public Role Position { get; set; }
+        public Adventure[] Adventures { get; }
+        public string WeaponOfChoice { get; set; }
+        public bool Evil { get; set; }
+
+        //Constructors
+        public Person()
+        {
+            //generates random person with random number of adventures
+        }
+        public Person(int numberOfAdventures)
+        {
+            //calls generate random person
+        }
+
+        public void AddAdventure(Adventure newAdventure)
+        {
+            //Add a new adventure to the adventures array
+        }
+
+        public void GenerateRandomPerson(int numberOfAdventures)
+        {
+            //randomly assign values to each of the attributes of person
+        }
+
+        public override string ToString()
+        {
+            //change to return each attribute of this class in a logical way
+            return base.ToString();
+        }
     }
 }
