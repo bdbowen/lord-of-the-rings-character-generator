f5bbd16cb11884d4f87419c995df03b6374fd2d6
Brian Bowen <brianbowen1999@gmail.com>
2020-02-26 11:45:55 -0600

    Finished the skeletal structure of Human Class

--- CharacterGenerator/Domain/Human.cs
+++ CharacterGenerator/Domain/Human.cs
@@ -6,7 +6,30 @@ namespace CharacterGenerator
 {
     class Human : Person
     {
-        //Will finish adding variables before Friday
+        //TODO add error checking to the getter and setters
+        //class attributes
+        public static int MAXAGE = 100;
+        public string AncestralLine { get; set; }
+        public Physical[] PhysicalAbilities { get; }
 
+        //constructors
+        public Human()
+        {
+            //instantiate object with random attributes and random number of abilities
+        }
+        public Human(int numberOfAbilities)
+        {
+            //instantiate object with random attributes, but a specified number of abilities
+        }
+
+        public void AddAbility(Physical newAbility)
+        {
+            //add the new ability to the PhysicalAbilities Array
+        }
+
+        public void GenerateRandomHuman(int numberOfAbilities)
+        {
+            //randomly assign each attribute a value
+        }
     }
 }
