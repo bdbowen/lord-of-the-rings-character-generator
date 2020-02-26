0c23afea311b89682416b5ea03badea48767c67e
Brian Bowen <brianbowen1999@gmail.com>
2020-02-26 09:46:37 -0600

    added another constructor to Person

--- CharacterGenerator/Domain/Person.cs
+++ CharacterGenerator/Domain/Person.cs
@@ -33,6 +33,13 @@ namespace CharacterGenerator
             //calls generate random person with specific number of adventures
         }
 
+        public Person(int numberOfAdventures, int maxAge, string[] firstNameOptions, string[] lastNameOptions)
+        {
+            //to be used mainly with Person's sub classes, this allows more control of randomization
+            //that way a wizard name sounds more wizardly and a human name sounds more human 
+            //and age is more logical - you don't get a 2000 year old human
+        }
+
         public void AddAdventure(Adventure newAdventure)
         {
             //Add a new adventure to the adventures array
