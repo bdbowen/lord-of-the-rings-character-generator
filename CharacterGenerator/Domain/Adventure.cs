8e5b3097b552453204bb4bbbca25cc3051e94155
Brian Bowen <brianbowen1999@gmail.com>
2020-02-28 16:20:24 -0600

    Finished laying out the Skeletal structure of the Adventure Class

--- CharacterGenerator/Domain/Adventure.cs
+++ CharacterGenerator/Domain/Adventure.cs
@@ -6,7 +6,30 @@ namespace CharacterGenerator
 {
     class Adventure
     {
-        //Will finish adding variables before Friday
+        public string LeaderName { get; set; }
+        public Location WhereTo { get; set; }
+        public string[] CompanionNames { get; set; }
+        public bool Successful { get; set; }
+        public bool Fatal { get; set; }
 
+        //constructors
+        public Adventure()
+        {
+            //Generate a random Adventure with a random number of companions
+        }
+
+        public Adventure(int numberOfCompanions)
+        {
+            //generate a random Adventure with set number of Adventures
+        }
+        public void GenerateRandomAdventure(int numberOfCompanions)
+        {
+            //randomly assign each variable a value
+        }
+        public override string ToString()
+        {
+            //return a more logical string with the class
+            return base.ToString();
+        }
     }
 }
