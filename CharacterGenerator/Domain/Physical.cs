71bf1ed2f0954dd06df2f1af263ee6dde04ff555
Brian Bowen <brianbowen1999@gmail.com>
2020-02-28 20:55:57 -0600

    Finished the Skeletal structure of the Physical Class

--- CharacterGenerator/Domain/Physical.cs
+++ CharacterGenerator/Domain/Physical.cs
@@ -6,6 +6,26 @@ namespace CharacterGenerator
 {
     class Physical : Ability
     {
-        //Will finish adding variables before Friday
+        public string ToolRequired { get; set; }
+        public string RaceOfOrigin { get; set; }
+        public Location TrainingLocation { get; set; }
+        public bool Fighting { get; set; }
+        public bool Lethal { get; set; }
+        public bool RequiresWeaponMastery { get; set; }
+
+        public Physical()
+        {
+            //generate random Physical object
+        }
+
+        public void GenerateRandomPhysical()
+        {
+            //assigns random values to each of the class attributes
+        }
+        public override string ToString()
+        {
+            //return a string that makes more sense with the Physical class
+            return base.ToString();
+        }
     }
 }
