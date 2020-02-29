630dfb70de211555b47abce72c1ecaf758843076
Brian Bowen <brianbowen1999@gmail.com>
2020-02-28 21:15:58 -0600

    Finished Skeletal Structure for Role Class

--- CharacterGenerator/Domain/Role.cs
+++ CharacterGenerator/Domain/Role.cs
@@ -6,6 +6,27 @@ namespace CharacterGenerator
 {
     class Role
     {
-        //Will finish adding variables before Friday
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string PrimaryRace { get; set; }
+        public int NumberOfSubordinates { get; set; }
+        public int LenthOfOccupancy { get; set; }
+        public bool SupremeRole { get; set; }
+
+        public Role()
+        {
+            //generate random role instance
+        }
+
+        public void GenerateRandomRole()
+        {
+            //assigns random values to each class attribute
+        }
+
+        public override string ToString()
+        {
+            //return a more logical string to fit with the Role class
+            return base.ToString();
+        }
     }
 }
