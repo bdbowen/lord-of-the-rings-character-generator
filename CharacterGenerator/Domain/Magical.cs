3c61eca51e143f401fb52e77e71c5860bf08fb93
Brian Bowen <brianbowen1999@gmail.com>
2020-02-28 21:03:43 -0600

    Finished the Skeletal Structure for the Mental and Magical Classes

--- CharacterGenerator/Domain/Magical.cs
+++ CharacterGenerator/Domain/Magical.cs
@@ -6,7 +6,24 @@ namespace CharacterGenerator
 {
     class Magical : Ability
     {
-        //Will finish adding variables before Friday
+        public string ElementType { get; set; }
+        public Location ArchiveLocation { get; set; }
+        public bool LightMagic { get; set; }
 
+        public Magical()
+        {
+            //generates random Magical Ability Object
+        }
+
+        public void GenerateRandomMagical()
+        {
+            //assigns random values to each attribute of this object
+        }
+
+        public override string ToString()
+        {
+            //returns a string that makes more sense with the class
+            return base.ToString();
+        }
     }
 }
--- CharacterGenerator/Domain/Mental.cs
+++ CharacterGenerator/Domain/Mental.cs
@@ -6,6 +6,23 @@ namespace CharacterGenerator
 {
     class Mental : Ability
     {
-        //Will finish adding variables before Friday
+        public string RaceOfOrigin { get; set; }
+        public Location SchoolLocation { get; set; }
+        public bool RequiresSubjectMastery { get; set; }
+
+        public Mental()
+        {
+            //generates random Mental object
+        }
+
+        public void GenerateRandomMental()
+        {
+            //assigns random values to each class attribute
+        }
+        public override string ToString()
+        {
+            //return a string that makes more sense for the Mental class
+            return base.ToString();
+        }
     }
 }
