874ff3e9f6f9f3e9d6a94f73fe6fe94174a9170b
Brian Bowen <brianbowen1999@gmail.com>
2020-02-28 21:12:27 -0600

    Finished Skeletal Structure for Location Class

--- CharacterGenerator/Domain/Location.cs
+++ CharacterGenerator/Domain/Location.cs
@@ -6,7 +6,27 @@ namespace CharacterGenerator
 {
     class Location
     {
-        //Will finish adding variables before Friday
+        public string Title { get; set; }
+        public string InhabitantsPrimaryRace { get; set; }
+        public string Region { get; set; }
+        public char RelativeMapPosition { get; set; } //N, S, E, or W
+        public bool FreeLand { get; set; }
+
+        public Location()
+        {
+            //generate random location
+        }
+
+        public void GenerateRandomLocation()
+        {
+            //assign random values to each class attribute
+        }
+
+        public override string ToString()
+        {
+            //return a string that makes more sense with Location Class
+            return base.ToString();
+        }
 
     }
 }
